1e40400febae6c8691f8a7ada437a5cd2c894fe0
CurlySoldier <73424198+CurlySoldier@users.noreply.github.com>
2023-09-16 22:15:18 +0200

    feat: Allowed blog information to be displayed on frontend if call to API is successful

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,25 +1,42 @@
 "use client";
-
+import * as React from "react";
 interface PostData {
   id: string;
   author: string;
-  title: string;
-  content: string;
+  heading: string;
+  contents: string;
   date: Date;
 }
 
-const callAPI = async () => {
-  const response = await fetch("http://localhost:5266/Article");
-  const data = await response.json();
-  return data;
-};
+const Posts: React.FC = () => {
+  const [posts, setPosts] = React.useState<PostData[]>([]);
 
-function Posts() {
+  const callAPI = async () => {
+    try {
+      const response = await fetch("http://localhost:5266/Article");
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const data = await response.json();
+      setPosts(data);
+    } catch (error) {
+      console.error("API call error:", error);
+    }
+  };
+
   return (
     <div>
       <button onClick={callAPI}>Make API call</button>
+      <div>
+        {posts.map((post: PostData) => (
+          <div key={post.id}>
+            <h3>{post.heading}</h3>
+            <p>{post.contents}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default Posts;
